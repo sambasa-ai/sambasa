e7e77e170ea5cae39b51843935e5c30c3d9e89fc
Sumit Gore <sumeetgore96@gmail.com>
2026-03-21 17:37:12 +0100

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Providers } from "@/components/providers";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Figtree } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Sambasa",
@@ -14,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn("font-sans", figtree.variable)}>
       <body className="antialiased">
         <Providers>{children}</Providers>
         <Toaster />
